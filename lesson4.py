5c9b51461bdacd26e027227271a3590d9347ec4b
Ashraf-DIU <ashrafbialam17@ggmail.com>
2025-01-16 08:03:20 +0600

    Adding Dictionary

---
+++ lesson4.py
@@ -0,0 +1,15 @@
+info = {
+    "key" : "value",
+    "name" : "Ashraf Bin Alam",
+    "learning" : "Coding",
+    "age" : 35,
+    "is_Adult" : True,
+    "marks" : [34.2, 52.5, 53.2],#List
+    12 : "Twelve"
+}
+
+info["surname"] = "Mitul" #assign new
+
+print(info)
+print(type(info))
+print(info["marks"])
---
+++ nested_dictionary.py
@@ -0,0 +1,11 @@
+student = {
+    "name" : "Ashraf Bin Alam",
+    "subject" : {
+        "phy" : 90,
+        "che" : 98,
+        "math" : 98
+    }
+}
+
+print(student)
+print(student["subject"]["phy"])
